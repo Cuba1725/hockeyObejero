6d34c02b26fbdde915ce4c4a2770faa5bcfbe469
Daniel Medina <79946104+Cuba1725@users.noreply.github.com>
2021-09-24 12:46:37 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const btnMenu = document.querySelector('.btnMenu');
+const menu =  document.querySelector('.menu');
+
+
+btnMenu.addEventListener('click', e => {
+    btnMenu.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+});
+
+
+///SWIPER CAROUSEL
+window.addEventListener('load', function(){
+    var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    loop: true,        
+    });      
+});
